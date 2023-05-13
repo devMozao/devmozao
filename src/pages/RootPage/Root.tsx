d3493fe4b9931fc989ce6a69b5c90c5de12f67ad
Diogo "Mozão" Fonseca <devmozao@outlook.com>
2023-05-13 04:06:07 -0300

    chore: add image and icons

--- src/pages/RootPage/Root.tsx
+++ src/pages/RootPage/Root.tsx
@@ -1,8 +1,44 @@
-import { FC } from 'react'
+import { FC, ReactNode, useState } from 'react'
 
-import { Center, Container, Paper, Text } from '@mantine/core'
+import { Anchor, Box, Center, Container, Flex, Image, Paper } from '@mantine/core'
+import {
+  IconBrandGithub,
+  IconBrandInstagram,
+  IconBrandLinkedin,
+  IconBrandTelegram,
+  IconBrandTwitter,
+} from '@tabler/icons-react'
 
 const Root: FC = (): JSX.Element => {
+  const [selected, setSelected] = useState<string>('')
+
+  type SocialIconsProps = {
+    children: ReactNode
+    url: string
+    selectName: string
+  }
+
+  const SocialIcons: FC<SocialIconsProps> = ({ children, url, selectName }): JSX.Element => {
+    return (
+      <Anchor
+        underline
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        sx={{
+          '&:hover': {
+            transform: 'scale(1.2)',
+            transition: 'transform 0.25s ease-in-out',
+          },
+        }}
+        onMouseEnter={() => setSelected(selectName)}
+        onMouseLeave={() => setSelected('')}
+      >
+        {children}
+      </Anchor>
+    )
+  }
+
   return (
     <Container
       fluid
@@ -11,7 +47,7 @@ const Root: FC = (): JSX.Element => {
       w="100%"
       h="100%"
       sx={{
-        backgroundColor: 'ivory',
+        backgroundColor: 'honeydew',
         overflow: 'hidden',
       }}
     >
@@ -23,11 +59,66 @@ const Root: FC = (): JSX.Element => {
           w={'calc(100vw - 20px)'}
           h={'calc(100vh - 20px)'}
           sx={{
-            background: 'linear-gradient(225deg, rgba(25,255,244,1) 0%, rgba(174,255,174,1) 100%)',
+            background: 'linear-gradient(45deg, rgba(25,255,244,1) 30%, rgba(174,255,174,1) 100%)',
           }}
         >
-          <Center w={'100%'} h={'100%'}>
-            <Text>Hello world</Text>
+          <Center w={'100%'} h={'calc(100% - 50px)'}>
+            <Container>
+              <Box>
+                <Image
+                  maw={355}
+                  mah={400}
+                  src={'/public/images/devmozao.webp'}
+                  sx={{
+                    pointerEvents: 'none',
+                    userSelect: 'none',
+                    WebkitUserSelect: 'none',
+                    msUserSelect: 'none',
+                  }}
+                />
+              </Box>
+              <Flex display="flex" justify="space-around" m="lg">
+                <SocialIcons url="https://www.twitter.com/devMozao/" selectName="twitter">
+                  <IconBrandTwitter
+                    size={44}
+                    stroke={2}
+                    color={selected === 'twitter' ? 'hotpink' : 'honeydew'}
+                  />
+                </SocialIcons>
+
+                <SocialIcons url="https://www.github.com/devMozao" selectName="github">
+                  <IconBrandGithub
+                    size={44}
+                    stroke={2}
+                    color={selected === 'github' ? 'hotpink' : 'honeydew'}
+                  />
+                </SocialIcons>
+
+                <SocialIcons url="https://www.linkedin.com/in/devmozao/" selectName="linkedin">
+                  <IconBrandLinkedin
+                    size={44}
+                    stroke={2}
+                    color={selected === 'linkedin' ? 'hotpink' : 'honeydew'}
+                  />
+                </SocialIcons>
+
+                <SocialIcons url="https://t.me/+wcol5DJoedRkOTAx" selectName="telegram">
+                  <IconBrandTelegram
+                    size={44}
+                    stroke={2}
+                    color={selected === 'telegram' ? 'hotpink' : 'honeydew'}
+                  />
+                </SocialIcons>
+
+                <SocialIcons url="https://www.instagram.com/devmozao" selectName="instagram">
+                  <IconBrandInstagram
+                    size={44}
+                    stroke={2}
+                    color={selected === 'instagram' ? 'hotpink' : 'honeydew'}
+                  />
+                </SocialIcons>
+              </Flex>
+            </Container>
           </Center>
         </Paper>
       </Center>
